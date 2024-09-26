492bcfadc61d9662f6d4594a47ada6190049f82e
mwilsch <marius.santiago.wilsch@gmail.com>
2024-09-26 14:54:52 +0200

    Feature list now updated like in miro

---
+++ src/constants/features.tsx
@@ -0,0 +1,22 @@
+import { IconCheckbox, IconChartBar, IconUsers } from "@tabler/icons-react";
+
+const features = [
+  {
+    title: "AI-Powered Check-Ins",
+    description:
+      "Daily voice messages keep students accountable and motivated.",
+    icon: <IconCheckbox />,
+  },
+  {
+    title: "Real-Time Analytics",
+    description: "Spot at-risk students before they drop out.",
+    icon: <IconChartBar />,
+  },
+  {
+    title: "Community Engagement",
+    description: "Foster peer support and collaboration effortlessly.",
+    icon: <IconUsers />,
+  },
+];
+
+export { features };

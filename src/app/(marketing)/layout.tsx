210f305ba84f78d2fb40b758d4ab48acecd0ea9b
mwilsch <marius.santiago.wilsch@gmail.com>
2024-09-26 13:13:53 +0200

    Locally is now running perfectly!

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../globals.css";
 import { GeistSans } from "geist/font/sans";
+import { NavBar } from "@/components/navbar";
 import { cn } from "@/lib/utils";
-import { ViewTransitions } from "next-view-transitions";
-import { ThemeProvider } from "@/context/theme-provider";
+import { Footer } from "@/components/footer";
 
 export const metadata: Metadata = {
   title: "Everything AI",
@@ -20,24 +20,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ViewTransitions>
-      <html lang="en">
-        <body
-          className={cn(
-            GeistSans.className,
-            "bg-white dark:bg-black antialiased h-full w-full"
-          )}
-        >
-          <ThemeProvider
-            attribute="class"
-            enableSystem
-            disableTransitionOnChange
-            defaultTheme="light"
-          >
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    </ViewTransitions>
+    <main>
+      <NavBar />
+      {children}
+      <Footer />
+    </main>
   );
 }

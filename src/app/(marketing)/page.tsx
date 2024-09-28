511ab67fe04ac642160a3b27ed0490cc56b36461
mwilsch <marius.santiago.wilsch@gmail.com>
2024-09-28 08:37:07 +0200

    Installed axios

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -12,13 +12,38 @@ import {
   type Testimonial,
   type MvCurrentKpis,
 } from "@/db";
-import fetchConcurrently from "@/hooks/fetch_concurrent";
-async function fetchData() {
-  // ? Is this risky because if one fails, the other one won't be fetched?
-  const testimonials = getTestimonials();
-  const currentKpis = getCurrentKpis();
-  return Promise.all([testimonials, currentKpis]);
-}
+
+// ! This is the best version but NextJS keeps throwing an DynamicServerError
+// async function fetchConcurrently(): Promise<{
+//   testimonials: Testimonial[];
+//   currentKpis: MvCurrentKpis | null;
+// }> {
+//   const testimonials = getTestimonials();
+//   const currentKpis = getCurrentKpis();
+
+//   const results = await Promise.allSettled([testimonials, currentKpis]);
+
+//   results.forEach((result, index) => {
+//     if (result.status === "rejected") {
+//       console.error(`Error in Promise ${index + 1}:`, result.reason);
+//     }
+//   });
+
+//   return {
+//     testimonials: results[0].status === "fulfilled" ? results[0].value : [],
+//     currentKpis: results[1].status === "fulfilled" ? results[1].value : null,
+//   };
+// }
+
+// export default fetchConcurrently;
+
+// ! This version works but Promise all is risky because if one fails, the other one won't be fetched
+// async function fetchData() {
+//   // ? Is this risky because if one fails, the other one won't be fetched?
+//   const testimonials = getTestimonials();
+//   const currentKpis = getCurrentKpis();
+//   return Promise.all([testimonials, currentKpis]);
+// }
 
 export default async function Home() {
   // const { testimonials, currentKpis } = await fetchConcurrently();

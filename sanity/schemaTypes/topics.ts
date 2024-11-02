cd8128c881e3de6b8062f114b0084c1b1e088855
Tom <aaa@libralab.ai>
2024-11-02 07:37:23 +0100

    topics

---
+++ sanity/schemaTypes/topics.ts
@@ -0,0 +1,18 @@
+// topic.ts
+export default {
+    name: 'topic',
+    title: 'Topic',
+    type: 'document',
+    fields: [
+      {
+        name: 'community',
+        title: 'Community',
+        type: 'string',
+      },
+      {
+        name: 'innovation',
+        title: 'Innovation',
+        type: 'string',
+      },
+    ],
+  };

23b743924e072e6b873440230b2ba9f193cad634
Tom <aaa@libralab.ai>
2024-11-02 11:55:08 +0100

    no img

--- src/components/blog-card.tsx
+++ src/components/blog-card.tsx
@@ -7,12 +7,9 @@ import Balancer from "react-wrap-balancer";
 import { Blog, urlFor } from "@/lib/blog";
 
 export const BlogCard = ({ blog }: { blog: Blog }) => {
-  // const truncate = (text: string, length: number) => {
-  //   return text.length > length ? text.slice(0, length) + "..." : text;
-  // };
   return (
     <Link
-      className="shadow-derek rounded-3xl border dark:border-neutral-800 w-full bg-white dark:bg-neutral-900  overflow-hidden  hover:scale-[1.02] transition duration-200"
+      className="shadow-derek rounded-3xl border dark:border-neutral-800 w-full bg-white dark:bg-neutral-900 overflow-hidden hover:scale-[1.02] transition duration-200"
       href={`/blog/${blog.currentSlug}`}
     >
       {blog.titleImage ? (
@@ -29,19 +26,11 @@ export const BlogCard = ({ blog }: { blog: Blog }) => {
         </div>
       )}
       <div className="p-4 md:p-8 bg-white dark:bg-neutral-900">
-        <div className="flex space-x-2 items-center  mb-2">
-          <Image
-            src={urlFor(blog.author.src).url() || ""}
-            alt={blog.author.name}
-            width={20}
-            height={20}
-            className="rounded-full h-5 w-5"
-          />
-          <p className="text-sm font-normal text-muted">{blog.author.name}</p>
-        </div>
+        {/* Blog Title */}
         <p className="text-lg font-bold mb-4">
           <Balancer>{blog.title}</Balancer>
         </p>
+        {/* Blog Small Description */}
         <p className="text-left text-sm mt-2 text-muted truncate">
           {blog.smallDescription}
         </p>

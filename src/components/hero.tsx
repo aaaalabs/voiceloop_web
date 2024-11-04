d133ba051914ccb6762171158aaf27781dd59797
Tom <aaa@libralab.ai>
2024-11-04 06:11:05 +0100

    meta

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -102,3 +102,11 @@ export const Hero = ({
     </div>
   );
 };
+
+export const metadata = {
+  title: "Signup - voiceloop | amplify what matters",
+  description: "Re-humanize your community with VoiceLoop. We use AI to cut the noise and amplify what matters - genuine human connections.",
+  openGraph: {
+    images: ["https://ik.imagekit.io/libralab/VoiceLoop/social"],
+  },
+};

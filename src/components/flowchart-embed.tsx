118f8eece41154b062d08b8bf2ec14d4c371bf7a
Tom <aaa@libralab.ai>
2024-11-10 07:22:32 +0100

    mobile updates

--- src/components/flowchart-embed.tsx
+++ src/components/flowchart-embed.tsx
@@ -20,7 +20,7 @@ export const FlowchartEmbed = () => {
       <div className="px-0 sm:px-8">
         <div className={`p-0 sm:p-4 border border-neutral-200 bg-neutral-100 dark:bg-neutral-800 dark:border-neutral-700 rounded-none sm:rounded-[32px] relative`}>
           <div className="relative bg-white dark:bg-white dark:border-neutral-700 border-0 sm:border border-neutral-200 rounded-none sm:rounded-[24px] overflow-hidden">
-            <div className="relative w-full" style={{ paddingBottom: isMobile ? "150%" : "calc(56.67% + 60px)" }}>
+            <div className="relative w-full" style={{ paddingBottom: isMobile ? "91%" : "calc(56.67% + 60px)" }}>
               <iframe
                 src={`/flowcharts/${isMobile ? 'mm_004_mobile.html' : 'mm_004.html'}?link_target=parent&embed=1&ui=min&toolbar=0&nav=0`}
                 className="absolute inset-0 w-full h-full rounded-none sm:rounded-xl border-0 sm:border border-neutral-200 dark:border-neutral-800 sm:py-7.5 sm:px-10"
